abe5921b3ae3b1a168b93c37de27668fced5a282
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-02 14:37:30 -0300

    feat: add login functionality

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -8,18 +8,10 @@ import { setCookie } from "hono/cookie"
 import { AUTH_COOKIE } from "../constants"
 
 const app = new Hono()
-  .post("/login", zValidator("json", loginSchema), (c) => {
+  .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json")
 
-    console.log({ email, password })
-
-    return c.json({ email, password })
-  })
-  .post("/register", zValidator("json", registerSchema), async (c) => {
-    const { email, name, password } = c.req.valid("json")
-
     const { account } = await createAdminClient()
-    const user = await account.create(ID.unique(), email, password, name)
     const session = await account.createEmailPasswordSession(email, password)
 
     setCookie(c, AUTH_COOKIE, session.secret, {
@@ -30,7 +22,26 @@ const app = new Hono()
       maxAge: 60 * 60 * 24 * 30,
     })
 
-    return c.json({ data: user })
+    return c.json({ success: true })
+  })
+  .post("/register", zValidator("json", registerSchema), async (c) => {
+    const { email, name, password } = c.req.valid("json")
+
+    const { account } = await createAdminClient()
+
+    await account.create(ID.unique(), email, password, name)
+
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    return c.json({ success: true })
   })
 
 export default app
