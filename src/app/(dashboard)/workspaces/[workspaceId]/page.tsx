b3af087899d7ecd535646b8e78d6bfc2d7d818dc
Iraildo Silva <iraildosilva1999@gmail.com>
2024-11-08 19:42:13 -0300

    feat: add protection to workspaces routes

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,10 @@
-export default function WorkspaceIdPage() {
-	return (
-		<div>WorkspaceId</div>
-	)
-};
+import { redirect } from 'next/navigation'
+
+import { getCurrent } from '@/features/auth/actions'
+
+export default async function WorkspaceIdPage() {
+  const user = await getCurrent()
+  if (!user) redirect('/sign-in')
+
+  return <div>WorkspaceId</div>
+}
