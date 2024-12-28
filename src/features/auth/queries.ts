1dbcf7847f57966c9e7d51011adc19451b9544b8
Iraildo Silva <iraildosilva1999@gmail.com>
2024-12-28 14:20:26 -0300

    feat: implement createSessionClient

--- src/features/auth/queries.ts
+++ src/features/auth/queries.ts
@@ -1,21 +1,9 @@
-import { cookies } from 'next/headers'
-import { Account, Client } from 'node-appwrite'
-
-import { AUTH_COOKIE } from './constants'
+import { createSessionClient } from '@/lib/apprwrite'
 
 export async function getCurrent() {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
-
-    const session = await cookies().get(AUTH_COOKIE)
-
-    if (!session) return null
-
-    client.setSession(session.value)
-    const account = new Account(client)
-
+    const { account } = await createSessionClient()
+ 
     return await account.get()
   } catch {
     return null
