a64c279d2c440eaaeed9cb4c681506cc9b59e5fb
Iraildo Silva <iraildosilva1999@gmail.com>
2025-01-01 12:11:24 -0300

    feat: add delete member api route

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -8,20 +8,68 @@ import { sessionMiddleware } from '@/lib/session-middleware'
 import { DATABASE_ID, MEMBERS_ID } from '@/config'
 
 import { getMember } from '../utils'
+import { MemberRole } from '../types'
 
-const app = new Hono().get(
-  '/',
-  sessionMiddleware,
-  zValidator('query', z.object({ workspaceId: z.string() })),
-  async (c) => {
-    const { users } = await createAdminClient()
+const app = new Hono()
+  .get(
+    '/',
+    sessionMiddleware,
+    zValidator('query', z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const { users } = await createAdminClient()
+      const databases = c.get('databases')
+      const user = c.get('user')
+      const { workspaceId } = c.req.valid('query')
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      })
+
+      if (!member) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+        Query.equal('workspaceId', workspaceId),
+      ])
+
+      const populatedMembers = await Promise.all(
+        members.documents.map(async (member) => {
+          const user = await users.get(member.userId)
+
+          return {
+            ...member,
+            name: user.name,
+            email: user.email,
+          }
+        })
+      )
+
+      return c.json({ data: { ...members, documents: populatedMembers } })
+    }
+  )
+  .delete('/:memberId', sessionMiddleware, async (c) => {
+    const { memberId } = c.req.param()
     const databases = c.get('databases')
     const user = c.get('user')
-    const { workspaceId } = c.req.valid('query')
+
+    const memberToDelete = await databases.getDocument(
+      DATABASE_ID,
+      MEMBERS_ID,
+      memberId
+    )
+
+    const allMembersInTheWorkspace = await databases.listDocuments(
+      DATABASE_ID,
+      MEMBERS_ID,
+      [Query.equal('workspaceId', memberToDelete.workspaceId)]
+    )
 
     const member = await getMember({
       databases,
-      workspaceId,
+      workspaceId: memberToDelete.workspaceId,
       userId: user.$id,
     })
 
@@ -29,24 +77,13 @@ const app = new Hono().get(
       return c.json({ error: 'Unauthorized' }, 401)
     }
 
-    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
-      Query.equal('workspaceId', workspaceId),
-    ])
+    if (member.$id !== memberToDelete.$id && member.role !== MemberRole.ADMIN) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
 
-    const populatedMembers = await Promise.all(
-      members.documents.map(async (member) => {
-        const user = await users.get(member.userId)
+    await databases.deleteDocument(DATABASE_ID, MEMBERS_ID, memberId)
 
-        return {
-          ...member,
-          name: user.name,
-          email: user.email,
-        }
-      })
-    )
-
-    return c.json({ data: { ...members, documents: populatedMembers } })
-  }
-)
+    return c.json({ data: { $id: memberToDelete.$id } })
+  })
 
 export default app
