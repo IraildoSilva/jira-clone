9c1dc02f9b0a5cfe503731bb5789b3718fa283ce
Iraildo Silva <iraildosilva1999@gmail.com>
2025-01-06 09:48:54 -0300

    feat: add sidebar projects session

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image"
-import Link from "next/link"
-import { DottedSeparator } from "./dotted-separator"
-import { Navigation } from "./navigation"
-import { WorkspaceSwitcher } from "./workspace-switcher"
+import Image from 'next/image'
+import Link from 'next/link'
+import { DottedSeparator } from './dotted-separator'
+import { Navigation } from './navigation'
+import { WorkspaceSwitcher } from './workspace-switcher'
+import { Projects } from './projects'
 
 export function Sidebar() {
   return (
     <div className="h-full bg-neutral-100 p-4 w-full">
-      <Link href={"/"}>
+      <Link href={'/'}>
         <Image src="/logo.svg" alt="logo" width={164} height={48} />
       </Link>
 
@@ -16,7 +17,12 @@ export function Sidebar() {
       <WorkspaceSwitcher />
 
       <DottedSeparator className="my-4" />
+
       <Navigation />
+
+      <DottedSeparator className="my-4" />
+
+      <Projects />
     </div>
   )
 }
