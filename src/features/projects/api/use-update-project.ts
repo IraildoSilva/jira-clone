5013f732ff7a032ce4cce37120f5a76fcb421aed
Iraildo Silva <iraildosilva1999@gmail.com>
2025-01-09 11:01:41 -0300

    feat: add update project api route

---
+++ src/features/projects/api/use-update-project.ts
@@ -0,0 +1,42 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { InferRequestType, InferResponseType } from 'hono'
+
+import { client } from '@/lib/rpc'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+
+type Route = (typeof client.api.projects)[':projectId']['$patch']
+
+type ResponseType = InferResponseType<Route, 200>
+type RequestType = InferRequestType<Route>
+
+export function useUpdateProject() {
+  const router = useRouter()
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.projects[':projectId']['$patch']({
+        form,
+        param,
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to update project')
+      }
+
+      return await response.json()
+    },
+    onSuccess: ({ data }) => {
+      toast.success('Project updated')
+      router.refresh()
+      queryClient.invalidateQueries({ queryKey: ['projects'] })
+      queryClient.invalidateQueries({ queryKey: ['projects', data.$id] })
+    },
+    onError: () => {
+      toast.error('Failed to update project')
+    },
+  })
+
+  return mutation
+}
