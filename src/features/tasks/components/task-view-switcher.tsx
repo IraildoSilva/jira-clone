0b549d2a520c480ded8b72d2853fcb63048435f8
Iraildo Silva <iraildosilva1999@gmail.com>
2025-01-10 12:44:40 -0300

    feat: add task view switcher url query state

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -5,12 +5,27 @@ import { Button } from '@/components/ui/button'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { PlusIcon } from 'lucide-react'
 import { useCreateTaskModal } from '../hooks/use-create-task-modal'
+import { useGetTasks } from '../api/use-get-tasks'
+import { useWorkspaceId } from '@/features/workspaces/hooks/use-workspace-id'
+import { useQueryState } from 'nuqs'
 
 export function TaskViewSwitcher() {
+  const [view, setView] = useQueryState('task-view', {
+    defaultValue: 'table',
+  })
+
   const { open } = useCreateTaskModal()
+  const workspaceId = useWorkspaceId()
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+  })
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      className="flex-1 w-full border rounded-lg"
+      defaultValue={view}
+      onValueChange={setView}
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -35,15 +50,15 @@ export function TaskViewSwitcher() {
         <DottedSeparator className="my-4" />
         <>
           <TabsContent value="table" className="mt-0">
-            Data table
+            {JSON.stringify(tasks)}
           </TabsContent>
 
           <TabsContent value="kanban" className="mt-0">
-            Data Kanban
+            {JSON.stringify(tasks)}
           </TabsContent>
 
           <TabsContent value="calendar" className="mt-0">
-            Data Calendar
+            {JSON.stringify(tasks)}
           </TabsContent>
         </>
       </div>
