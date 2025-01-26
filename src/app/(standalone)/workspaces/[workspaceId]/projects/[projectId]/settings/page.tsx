0f25390adb4be740e0faa88e74033a794cfc79ca
Iraildo Silva <iraildosilva1999@gmail.com>
2025-01-26 10:00:04 -0300

    feat: refactor project settings page

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,25 +1,12 @@
-import { getCurrent } from '@/features/auth/queries'
-import { EditProjectForm } from '@/features/projects/components/edit-project-form'
-import { getProject } from '@/features/projects/queries'
 import { redirect } from 'next/navigation'
 
-interface ProjectIdSettingsPageProps {
-  params: {
-    projectId: string
-  }
-}
+import { getCurrent } from '@/features/auth/queries'
 
-export default async function ProjectIdSettingsPage({
-  params,
-}: ProjectIdSettingsPageProps) {
+import { ProjectIdSettingsClient } from './client'
+
+export default async function ProjectIdSettingsPage() {
   const user = await getCurrent()
   if (!user) redirect('/sign-in')
 
-  const initialValues = await getProject({ projectId: params.projectId })
-
-  return (
-    <div className="w-full lg:max-w-xl">
-      <EditProjectForm initialValues={initialValues} />
-    </div>
-  )
+  return <ProjectIdSettingsClient />
 }
